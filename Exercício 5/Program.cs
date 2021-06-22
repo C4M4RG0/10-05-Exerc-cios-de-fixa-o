223e62b058910fbc8547b84ad9b171083926cad2
C4M4RG0 <82672609+C4M4RG0@users.noreply.github.com>
2021-06-22 18:21:34 -0300

    10/05- Exercícios de fixação

---
+++ Exercício 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio5___repetitiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para ver a tabuada");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tabuada do " + numero);
+
+			for (int x = 1; x <= 10; x++)
+            {
+                for (var o = 1; o <= 1; o++)
+                {
+                    Console.WriteLine(numero + " * " + x + " = " + numero * x);
+                }
+            }
+        }
+    }
+}
